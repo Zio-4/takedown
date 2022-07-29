e31f6f3a8b5d80f429c8ad0841beea96095fe8b5
Philip Ziolkowski <philipz4848@gmail.com>
2022-07-28 17:33:03 -0700

    Adding environment variable for API url

--- pages/notes/[id].jsx
+++ pages/notes/[id].jsx
@@ -14,7 +14,7 @@ export default Page
 
 export async function getServerSideProps({params, req, res}) {
 
-  const response = await fetch(`http://localhost:3000/api/note/${params.id}`)
+  const response = await fetch(`${process.env.API_URL}/api/note/${params.id}`)
   
   if (!response.ok) {
     res.writeHead(302, { Location: '/notes' }).end()
